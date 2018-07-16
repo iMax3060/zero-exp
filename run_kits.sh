94cafbf80f084e7dd5a3df69e6f4b02634b96e24
Caetano Sauer <csauer@cs.uni-kl.de>
2018-07-16 01:47:48 -0700

    Added iotop and df to run_kits command traces

--- run_kits.sh
+++ run_kits.sh
@@ -4,9 +4,11 @@ source functions.sh || (echo "functions.sh not found!"; exit)
 source config.sh || (echo "config.sh not found!"; exit)
 
 function clean_up {
-    kill $IOSTAT_PID > /dev/null 2>&1
-    kill $MPSTAT_PID > /dev/null 2>&1
-    kill $FREE_PID > /dev/null 2>&1
+    kill -9 $IOSTAT_PID > /dev/null 2>&1
+    kill -9 $MPSTAT_PID > /dev/null 2>&1
+    kill -9 $FREE_PID > /dev/null 2>&1
+    kill -9 $IOTOP_PID > /dev/null 2>&1
+    kill -9 $DF_PID > /dev/null 2>&1
 }
 
 RUN_GDB=false
@@ -17,12 +19,15 @@ fi
 
 iostat -dmtx 1 > iostat.txt 2> /dev/null &
 IOSTAT_PID=$!
-
 mpstat 1 > mpstat.txt 2> /dev/null &
 MPSTAT_PID=$!
-
+sudo -n iotop -qtaP | grep "zapps kits" | grep -v "grep" > iotop.txt 2> /dev/null &
+IOTOP_PID=$!
 free -m -c 100000 -s 1 > free.txt 2> /dev/null &
 FREE_PID=$!
+rm -f df.txt
+while true; do df -t ext4 >> df.txt; sleep 1; done &
+DF_PID=$!
 
 # Call clean_up when recieving one of these signals
 trap clean_up SIGHUP SIGINT SIGTERM
@@ -47,7 +52,7 @@ if [ -n "$ARCHDIR" ]; then
     KITS_OPTS+=" --sm_archdir $ARCHDIR/archive"
 fi
 
-CMD="zapps kits $KITS_OPTS $*"
+CMD="./zapps kits $KITS_OPTS $*"
 EXIT_CODE=0
 
 if $RUN_GDB; then
